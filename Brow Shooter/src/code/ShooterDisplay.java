99049f13bbe47dad91c96e781e77df2e583486c2
anishvisagit <anishvisaria98@gmail.com>
2015-08-07 23:25:40 -0400

    Made ShooterDisplay relative for android display

--- Brow Shooter/src/code/ShooterDisplay.java
+++ Brow Shooter/src/code/ShooterDisplay.java
@@ -6,13 +6,14 @@ import processing.core.PImage;
 public class ShooterDisplay extends PApplet {
 
 	private PImage base, title, start, options, hanger, leaderboard, left_brow, right_brow;
-	private float tint = 0;
-	private float titleX = width/2, titleY = -180;
-	private float titleSpeed = 10f;
-	private int startXPos = -200, optionsXPos = 200;
+	private float tint = 0f;
+	private float titleX, titleY;
+	private float titleSpeed, menu_speed;
+	private int startXPos, optionsXPos;
 
-	public void setup() {
+	public void setup() {		
 		size(400, 600);
+		// size(displayWidth, displayHeight); // for android
 		background(0);
 		base = loadImage("Brow Fighte base.png");
 		title = loadImage("Brow Fighter title.png");
@@ -22,7 +23,7 @@ public class ShooterDisplay extends PApplet {
 		leaderboard = loadImage("Brow Fighterleaderboard second edition.png");
 		right_brow = loadImage("Brow Fighter default right brow.png");
 		left_brow = loadImage("Brow Fighter default left brow.png");
-		//System.out.println(base.loaded);
+		
 		base.resize(width, height);
 		title.resize(width, height);
 		start.resize(width, height);
@@ -31,11 +32,18 @@ public class ShooterDisplay extends PApplet {
 		leaderboard.resize(width, height);
 		right_brow.resize(width, height);
 		left_brow.resize(width, height);
+
+		titleX = 0;
+		titleY = (-180f/600)*height;
+		titleSpeed = (20f/600)*height;
+		startXPos = -width/2;
+		optionsXPos = width/2;
+		menu_speed = (float) ((20.0/400)*width);
 	}
 
 	public void draw() {
 		if (tint < 10)	{
-			tint += 0.08;
+			tint += 0.2;
 			tint(255, tint);
 			image(base, 0,0);
 		}
@@ -55,8 +63,8 @@ public class ShooterDisplay extends PApplet {
 			image(left_brow, startXPos, 0);
 			image(right_brow, optionsXPos, 0);
 			if (startXPos != 0 && optionsXPos != 0) {
-				startXPos += 10;
-				optionsXPos -= 10;
+				startXPos += menu_speed;
+				optionsXPos -= menu_speed;
 			}
 		}
 
